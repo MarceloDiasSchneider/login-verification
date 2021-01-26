f81dec662e4a12db33ead56b5d568233b5eed125
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-01-26 15:40:29 +0100

    Update session_verify.php

--- php/session_verify.php
+++ php/session_verify.php
@@ -6,10 +6,10 @@
             $msg1 = '<h3>Keep working '.$_SESSION['name'].'</h3>';
             $msg2 = "<p>You are still logged in!</p>";
         } else {
-            header("Location: close_session.php");
+            header("Location: session_close.php");
         }
     } else {  
-        header("Location: close_session.php");
+        header("Location: session_close.php");
     }
 
 ?>  
